ca9a2342db652771be64622d7489c285b95e1712
Сергей Огер <geniuserii85@gmail.com>
2019-09-21 06:44:49 +0300

    Delete  folder ByteCode

--- ByteCode/ByteCodeCompilation.cmd
+++
@@ -1,4 +0,0 @@
-javac -d bin src/com/zmex/byteCodeExample/user/User.java
-javac -sourcepath ./src -d bin src/com/zmex/byteCodeExample/ByteCodeExample.java
-java -classpath ./bin com/zmex/byteCodeExample/ByteCodeExample
-javap -v bin/com/zmex/byteCodeExample/ByteCodeExample
