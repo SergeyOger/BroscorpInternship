c4a756061a91c43065a0bb9586c3dcdd5da0d426
Sergey Oger <geniuserii85@gmail.com>
2019-10-20 20:37:37 +0300

    Add additional fixes to SQL exercises

--- HomeTask/src/main/resources/sqlExamples/cats/exercises.sql
+++ HomeTask/src/main/resources/sqlExamples/cats/exercises.sql
@@ -1,6 +1,10 @@
 select *
 from cats
-where age > 2;
+order by name;
+
+select *
+from kittens
+order by kittenname;
 
 select kittenname, c.name
 from kittens
@@ -37,7 +41,8 @@ from (select count(*) as counter from kittens group by catname) as result;
 select avg(counter) as avg_kittens_amount
 from (select count(*) as counter from kittens group by catname) as result;
 
-
+select catname, (select count(*) from kittens group by catname)
+from kittens;
 
 select cat_name, kittens_number, min_number_of_kittens
 from (select name                                                             as cat_name,
@@ -60,5 +65,51 @@ from (select name                                                             as
 where kittens_number = min_number_of_kittens;
 
 
+
 select min(counter) as min_value
 from (select count(*) counter from kittens group by catname) result;
+
+select catname, count(*) as kittens_counter
+from kittens
+group by catname
+order by catname;
+
+select avg(counters) as average_kittens_number
+from (select count(*) counters from kittens group by catname) as counters;
+
+select distinct catname
+from kittens
+order by catname;
+
+select catname, max(age)
+from kittens
+group by catname
+order by catname;
+
+select catname, min(age)
+from kittens
+group by catname
+order by catname;
+
+select catname, max(age) - min(age) as kittens_age_difference
+from kittens
+group by catname
+order by catname;
+
+select name, age as cat_age, age - min_kittens_age as difference, min_kittens_age
+from (select name,
+             age,
+             (select min(age) from kittens group by cats.age) as min_kittens_age
+      from cats) as result
+order by name;
+
+select catname, kittens_average_age
+from (select catname, avg(age) kittens_average_age from kittens group by catname) as average_age_of_kittens
+where kittens_average_age > 2
+group by catname, kittens_average_age
+order by catname;
+
+select catname, avg(age) kittens_average_age
+from kittens
+group by catname
+having avg(age) > 2;
